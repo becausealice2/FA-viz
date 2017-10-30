aba0d32017d0694dbd9d0d1f92aad4ddb35ce325
Alice Jiang <becausealice@gmail.com>
2017-10-29 18:59:24 -0600

    Complete map functionality

--- soda/soda_franchise_map_1.js
+++ soda/soda_franchise_map_1.js
@@ -6,10 +6,10 @@ var config  = {},
 config.div_id = "soda_franchise_map",
 config.map_shift_horizontal = 2,
 config.map_shift_vertical = 2,
-config.states = [04, 16, 32, 49];
+config.states = ["Arizona", "Idaho", "Nevada", "Utah"];
 
 d3.queue()
-	.defer(d3.json, "https://d3js.org/us-10m.v1.json")
+	.defer(d3.json, "https://gist.githubusercontent.com/michellechandra/0b2ce4923dc9b5809922/raw/a476b9098ba0244718b496697c5b350460d32f99/us-states.json")
 	.defer(d3.csv, "https://rawgit.com/becausealice2/FA-viz/master/soda/soda_data.csv")
 	.awaitAll(render_map);
 
@@ -21,29 +21,44 @@ function render_map(error, result_data){
 	var topology  = result_data[0],
 			locations = result_data[1];
 
-	var states = topology["objects"]["states"]
-
-	// Filter through all states to isolate selected states
-	states["geometries"] = states["geometries"].filter(function select_states(state_obj){
-																									return config.states.filter(function(state_id){
-																										return state_id == state_obj["id"];
-																									}).length !== 0;
-																								});
-
-	// Use world map's bounding box array to calculate the map's aspect ratio
-	var bbox             = topojson.bbox(topology),
-			map_aspect_ratio = (bbox[2]-bbox[0]) / (bbox[3]-bbox[1]);
-
 	// Get target element's width and use aspect ratio to set height
 	width  = document.getElementById(config.div_id).clientWidth,
-	height = width/map_aspect_ratio,
+	height = width * (2/3),
 	// Set margins around rendered map
 	margins.top    = 0,
 	margins.bottom = 0,
 	margins.left   = 0,
 	margins.right  = 0;
 
-	// Select target element and attach <svg> and <g> elements
+	var projection = d3.geoAlbersUsa().translate([width/2.75, height/2.75]);
+	// Geo-paths take a GeoJSON geometry/feature object and generate an SVG path data string or render the path to a Canvas
+	var path = d3.geoPath().projection(projection);
+
+	// Filter through all states to isolate selected states
+	topology["features"] = topology["features"].filter(function select_states(state_obj){
+																									return config.states.filter(function(state_name){
+																										return state_name == state_obj["properties"]["name"];
+																									}).length !== 0;
+																								});
+
+	var min_year = d3.min(locations, function(d) { return d["Opening year"]; }),
+			max_year = d3.max(locations, function(d) { return d["Opening year"]; });
+	
+	// Select target element
+  var svg = d3.select("#"+config.div_id)
+
+  // Slider
+	svg.append("input")
+				.attr("type", "range")
+				.attr("min", min_year)
+				.attr("max", max_year)
+				.attr("step", "1")
+				.attr("id", "year")
+				.on("input", function input() {
+					update();
+				});
+
+  // Select target element and attach <svg> and <g> elements
 	var svg = d3.select("#"+config.div_id)
 		.append("svg")
 			// Set SVG element's top left corner and width/height attributes
@@ -53,20 +68,12 @@ function render_map(error, result_data){
 			// Group together map paths and location markers	
 		.append('g')
 			.attr('class', config.div_id+"_group");
-
-
-
-	var projection = d3.geoAlbersUsa();
-	// Geo-paths take a GeoJSON geometry/feature object and generate an SVG path data string or render the path to a Canvas
-	var path = d3.geoPath();
-
-	topology = topojson.feature(topology,states).features;
+	
 	// Group together country shape paths and enter data
 	svg.append("g")
 			 .attr("class", config.div_id+"_states")
 			 .selectAll("path")
-			 // Convert TopoJSON file to GeoJSON for rendering
-			 .data(topology)
+			 .data(topology.features)
 			 .enter()
 			 // Render and style map
 			 .append("path")
@@ -75,20 +82,47 @@ function render_map(error, result_data){
 				 .style("stroke", "#000")
 				 .style("stroke-width", "1px");
 
+	// Group together location markers
+	var loc = svg.append("g").attr("class", config.div_id+"_locations");
+
 	// Variables for reuse
 	var lon = "Longitude",
 			lat = "Latitude";
 
-	console.log(locations);
+	// function location_color(location){
+	// 	switch (location["Company"]){
+	// 		case "Swig 'n Sweets":
+	// 			return "yellow";
+	// 		case 'FiiZ':
+	// 			return "blue";
+	// 		case 'Sodalicious':
+	// 			return "red";
+	// 	}
+	// }
 
-// Render and style circle location marker for each observation in reviews dataset
-svg.selectAll("circle")
-   .data(locations)
-   .enter()
-   .append("circle")
-     .attr("cx", function(d) { return projection([d[lon], d[lat]])[0]; })
-     .attr("cy", function(d) { return projection([d[lon], d[lat]])[1]; })
-     .attr("r", 1)
-     .style("fill", "blue");
+	function update(){
+
+		var slider_year = document.getElementById("year").value;
+
+		var new_loc = locations.filter(function filter_by_year(d){ if (d["Opening year"] <= slider_year) { return true; } });
+
+		// Render and style circle location marker for each observation in reviews dataset
+		var markers = loc.selectAll("circle");
+		var data = markers.data(new_loc);
+		var enter = data.enter(),
+				exit = data.exit();
+			 
+
+			enter.append("circle")
+			 			 .attr("class", config.div_id+"_location_markers")
+						 .attr("cx", function(d) { return projection([d[lon], d[lat]])[0]; })
+						 .attr("cy", function(d) { return projection([d[lon], d[lat]])[1]; })
+						 .attr("r", 2)
+						 .style("fill", "steelblue");
+
+		exit.remove();			
+	}
+
+	update();
 
 }; // Close d3.json("https://d3js.org/us-10m.v1.json", function render_map(topology){
