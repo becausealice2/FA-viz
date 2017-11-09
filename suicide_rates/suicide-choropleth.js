6e35e78e81397e5930f3a846bbf8457692306719
Alice Jiang <becausealice@gmail.com>
2017-11-08 17:33:34 -0700

    Add legend

--- suicide_rates/suicide-choropleth.js
+++ suicide_rates/suicide-choropleth.js
@@ -16,6 +16,10 @@ function ready(error, utah, suicide_rates){
 	var min = d3.min(suicide_rates, function(d){ return +d.rate_per_100k; }),
 			max = d3.max(suicide_rates, function(d){ return +d.rate_per_100k; });
 
+	var x = d3.scaleLinear()
+						.domain([min, max])
+						.rangeRound([600, 860]);
+
 	var color = d3.scaleQuantize()
 								.domain([min, max])
 								.range(d3.schemeReds[9]);
@@ -37,6 +41,40 @@ function ready(error, utah, suicide_rates){
 			// Group together map paths and location markers	
 		.append('g')
 			.attr('class', div_id + "_group");
+
+			var g = svg.append("g")
+		.attr("class", "key")
+		.attr("transform", "translate(0,40)");
+
+	g.selectAll("rect")
+		.data(color.range().map(function(d) {
+			d = color.invertExtent(d);
+			if (d[0] == null) d[0] = x.domain()[0];
+			if (d[1] == null) d[1] = x.domain()[1];
+			return d;
+			}))
+		.enter()
+		.append("rect")
+			.attr("height", 8)
+			.attr("x", function(d) { return x(d[0]); })
+			.attr("width", function(d) { return x(d[1]) - x(d[0]); })
+			.attr("fill", function(d) { return color(d[0]); });
+
+	g.append("text")
+		.attr("class", "caption")
+		.attr("x", x.range()[0])
+		.attr("y", -6)
+		.attr("fill", "#000")
+		.attr("text-anchor", "start")
+		.attr("font-weight", "bold")
+		.text("Suicide rate");
+
+	g.call(d3.axisBottom(x)
+						.tickSize(13)
+						.tickFormat(function(x, i) { return Math.floor(x) + "%"; })
+						.tickValues([min, (min+((max-min)/2)), max]))
+	 .select(".domain")
+		.remove();
 
 	// Group together country shape paths and enter data
 	svg.append("g")
