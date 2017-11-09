5ebba8dfe696b8e11b17ef19f6ed7c7ce45685d0
Alice Jiang <becausealice@gmail.com>
2017-11-08 22:40:12 -0700

    Adjust for Ghost

--- suicide_rates/suicide-choropleth.js
+++ suicide_rates/suicide-choropleth.js
@@ -1,12 +1,12 @@
 // Get target element's width and use aspect ratio to set height
 var div_id  = "choropleth",
 		width   = document.getElementById(div_id).clientWidth,
-		height  = width*0.6,
+		height  = width*0.7,
 		center  = [-113.3010264, 39.7287941],
 		// Set margins around rendered map
 		margins = {"top": 0, "bottom": 0, "left": 0, "right": 0};
 
-var projection = d3.geoMercator().scale(5000).center(center).translate([width/2.85, height/2.5]);
+var projection = d3.geoMercator().scale(5000).center(center).translate([width/2.77, height/2.5]);
 // Geo-paths take a GeoJSON geometry/feature object and generate an SVG path data string or render the path to a Canvas
 var path = d3.geoPath().projection(projection);
 
